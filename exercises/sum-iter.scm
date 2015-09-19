984360e4913cd065c00c18c229159885e1c45669
Jonas Collberg <jonas.collberg@gmail.com>
2015-09-19 20:04:31 +0200

    finished exercise 1.30

---
+++ exercises/sum-iter.scm
@@ -0,0 +1,10 @@
+#lang planet neil/sicp
+
+;;; exercise 1.30
+
+(define (sum-iter term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
