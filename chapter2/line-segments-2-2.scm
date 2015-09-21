4603c90d80b70dcb2dd53a194e64491080c192c6
Jonas Collberg <jonas.collberg@gmail.com>
2015-09-21 23:35:40 +0200

    Finished exercise 2.2.

---
+++ chapter2/line-segments-2-2.scm
@@ -0,0 +1,33 @@
+(load "../chapter1/common.scm")
+
+(define (make-segment start-point end-point)
+  (cons start-point end-point))
+
+(define (start-segment seg)
+  (car seg))
+
+(define (end-segment seg)
+  (cdr seg))
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point p)
+  (car p))
+
+(define (y-point p)
+  (cdr p))
+
+(define (midpoint-segment seg)
+  (make-point (average (x-point (start-segment seg))
+                       (x-point (end-segment seg)))
+              (average (y-point (start-segment seg))
+                       (y-point (end-segment seg)))))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
