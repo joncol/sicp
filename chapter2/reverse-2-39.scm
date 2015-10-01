321df5887664bec83d5f750418e02bea1e85428a
Jonas Collberg <jonas.collberg@gmail.com>
2015-10-01 20:04:44 +0200

    Solved exercise 2.39: reverse in terms of folds.

---
+++ chapter2/reverse-2-39.scm
@@ -0,0 +1,9 @@
+(load "fold-2-38.scm")
+
+;;; exercise 2.39
+
+(define (reverse1 sequence)
+  (fold-right (lambda (x y) (append y (list x))) nil sequence))
+
+(define (reverse2 sequence)
+  (fold-left (lambda (x y) (cons y x)) nil sequence))
