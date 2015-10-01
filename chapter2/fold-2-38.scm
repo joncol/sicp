c5ac85fa4177bd7849b18596dc9f6f8537fb31ef
Jonas Collberg <jonas.collberg@gmail.com>
2015-10-01 19:53:33 +0200

    Solved exercise 2.38.

---
+++ chapter2/fold-2-38.scm
@@ -0,0 +1,24 @@
+;;; exercise 2.38
+
+(load "accumulate.scm")
+(load "../chapter1/common.scm")
+
+(define fold-right accumulate)
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(fold-right / 1 (list 1 2 3))      ;; => ((3/1)/2)/1 = 3/2
+
+(fold-left / 1 (list 1 2 3))       ;; => ((1/1)/2)/3 = 1/6
+
+(fold-right list nil (list 1 2 3)) ;; => (1 (2 (3 nil)))
+
+(fold-left list nil (list 1 2 3))  ;; => (((nil 1) 2) 3)
+
+;;; fold-left is equal to fold-right if the operation is symmetric
