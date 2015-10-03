961c9909e3426b866e03c2189c4397ac71adbadc
Jonas Collberg <jonas.collberg@gmail.com>
2015-10-03 12:46:49 +0200

    Exercise 2.42: simplified solution.
    This version contains debug information.

--- chapter2/queens-2-42.scm
+++ chapter2/queens-2-42.scm
@@ -11,41 +11,45 @@
         (filter
          (lambda (positions) (safe? k positions))
          (flatmap
-          (lambda (rest-of-queens) ;; roq = a list of lists of positions
+          (lambda (rest-of-queens) ;; roq = a list position pairs
+            (newline)
+            (display "## rest-of-queens: ")
+            (display rest-of-queens)
+            (newline)
             (map (lambda (new-row)
                    (adjoin-position new-row k rest-of-queens))
                  (enumerate-interval 1 board-size)))
           (queen-cols (dec k)))))) ;; ways to position queens in k-1 columns
   (queen-cols board-size))
 
-(define empty-board (list nil))
+(define empty-board nil)
 
-;;; positions is a list of lists of (row col) positions.
-;;; For each list of positions:
-;;;   check the first (row col) in the sublist against previous
-;;;   (row col)-pairs for conflicts.
+;;; The argument positions is a list of position pairs (row col).
+;;; Check the first (row col) in the list against previous
+;;; (row col)-pairs for conflicts.
 ;;; TODO: refactor using hof?
 (define (safe? col positions)
-  (define (iter-safe? positions-left)
-    (define (conflicts-with? new-pos)
-      (lambda (pos)
-        (let ((r1 (pos-row pos))
-              (c1 (pos-col pos))
-              (r2 (pos-row new-pos))
-              (c2 (pos-col new-pos)))
-          (or (= r1 r2)
-              (= c1 c2)
-              (let ((slope (/ (- r2 r1) (- c2 c1))))
-                (or (= slope 1)
-                    (= slope -1)))))))
-    (if (null? positions-left)
-        #t
-        (let ((new-p (car (car positions-left)))
-              (old-ps (cdr (car positions-left))))
-          (if (any? (conflicts-with? new-p) old-ps)
-              #f
-              (iter-safe? (cdr positions-left))))))
-  (iter-safe? positions))
+  (newline)
+  (display "-> safe")
+  (display "  positions: ")
+  (display positions)
+  (newline)
+
+  (define (conflicts-with? new-pos)
+    (lambda (pos)
+      (let ((r1 (pos-row pos))
+            (c1 (pos-col pos))
+            (r2 (pos-row new-pos))
+            (c2 (pos-col new-pos)))
+        (or (= r1 r2)
+            (= c1 c2)
+            (let ((slope (/ (- r2 r1) (- c2 c1))))
+              (or (= slope 1)
+                  (= slope -1)))))))
+
+  (let ((new-p (car positions))
+        (old-ps (cdr positions)))
+    (not (any? (conflicts-with? new-p) old-ps))))
 
 (define (any? predicate sequence)
   (not (null? (filter predicate sequence))))
@@ -60,6 +64,11 @@
   (cadr pos))
 
 (define (adjoin-position row col positions)
-  (map (lambda (pos)
-         (cons (make-pos row col) pos))
-       positions))
+  (newline)
+  (display "    positions before: ")
+  (display positions)
+  (newline)
+  (display "    positions after: ")
+  (display (cons (make-pos row col) positions))
+  (newline)
+  (cons (make-pos row col) positions))
