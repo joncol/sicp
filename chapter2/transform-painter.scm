dffffa1a0c18f2783d895cfa7dd01c42d8b13043
Jonas Collberg <jonas.collberg@gmail.com>
2015-10-04 00:01:30 +0200

    Solved exercise 2.50.

---
+++ chapter2/transform-painter.scm
@@ -0,0 +1,10 @@
+(load "vector-2-46.scm")
+
+(define (transform-painter painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter
+         (make-frame new-origin
+                     (sub-vect (m corner1) new-origin)
+                     (sub-vect (m corner2) new-origin)))))))
