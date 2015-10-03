a3eff6b66286fc51ed72c101120e22a9e9e27e76
Jonas Collberg <jonas.collberg@gmail.com>
2015-10-03 21:38:31 +0200

    Solved exercises 2.46 and 2.47.

---
+++ chapter2/vector-2-46.scm
@@ -0,0 +1,21 @@
+;;; exercise 2.46
+
+(define (make-vect x y)
+  (cons x y))
+
+(define (xcor-vect v)
+  (car v))
+
+(define (ycor-vect v)
+  (cdr v))
+
+(define (add-vect v w)
+  (make-vect (+ (xcor-vect v) (xcor-vect w))
+             (+ (ycor-vect v) (ycor-vect w))))
+
+(define (sub-vect v w)
+  (make-vect (- (xcor-vect v) (xcor-vect w))
+             (- (ycor-vect v) (ycor-vect w))))
+
+(define (scale-vect v s)
+  (make-vect (* (xcor-vect v) s) (* (ycor-vect v) s)))
