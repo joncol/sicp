088cb7a9107e9004ace8ab33228d80d06dddf6ee
Jonas Collberg <jonas.collberg@gmail.com>
2015-10-04 21:44:19 +0200

    Solved exercise 2.56.

---
+++ chapter2/diff-data.scm
@@ -0,0 +1,59 @@
+;;; example 2.3.2: symbolic differentiation, data implementation
+
+(load "diff.scm")
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+
+(define (addend s)
+  (cadr s))
+
+(define (augend s)
+  (caddr s))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+
+(define (multiplier p)
+  (cadr p))
+
+(define (multiplicand p)
+  (caddr p))
+
+;;; exercise 2.56
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+
+(define (base e)
+  (cadr e))
+
+(define (exponent e)
+  (caddr e))
+
+(define (make-exponentiation base exp)
+  (cond ((= exp 0) 1)
+        ((= exp 1) base)
+        ((not (number? exp)) (error "exponent must be a number" exp))
+        (else (list '** base exp))))
