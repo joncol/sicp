69ec3c95dffb57b15ed6b9c485d23800d609be64
Jonas Collberg <jonas.collberg@gmail.com>
2015-10-04 23:38:43 +0200

    Reordered some code.

--- chapter2/diff-data.scm
+++ chapter2/diff-data.scm
@@ -10,18 +10,13 @@
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
 
+;;; sum
+
 (define (make-sum a1 a2)
   (cond ((=number? a1 0) a2)
         ((=number? a2 0) a1)
         ((and (number? a1) (number? a2)) (+ a1 a2))
         (else (list '+ a1 a2))))
-
-(define (make-product m1 m2)
-  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
-        ((=number? m1 1) m2)
-        ((=number? m2 1) m1)
-        ((and (number? m1) (number? m2)) (* m1 m2))
-        (else (list '* m1 m2))))
 
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+)))
@@ -32,6 +27,15 @@
 (define (augend s)
   (caddr s))
 
+;;; product
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
 (define (product? x)
   (and (pair? x) (eq? (car x) '*)))
 
@@ -41,7 +45,13 @@
 (define (multiplicand p)
   (caddr p))
 
-;;; exercise 2.56
+;;; exponentiation
+
+(define (make-exponentiation base exp)
+  (cond ((= exp 0) 1)
+        ((= exp 1) base)
+        ((not (number? exp)) (error "exponent must be a number" exp))
+        (else (list '** base exp))))
 
 (define (exponentiation? x)
   (and (pair? x) (eq? (car x) '**)))
@@ -51,9 +61,3 @@
 
 (define (exponent e)
   (caddr e))
-
-(define (make-exponentiation base exp)
-  (cond ((= exp 0) 1)
-        ((= exp 1) base)
-        ((not (number? exp)) (error "exponent must be a number" exp))
-        (else (list '** base exp))))
