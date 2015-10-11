3352d79056eb9f8ec2d12174778b76ceee03bb3d
Jonas Collberg <jonas.collberg@gmail.com>
2015-10-11 22:00:21 +0200

    Solve exercise 2.63

---
+++ chapter2/tree-to-list-2-63.scm
@@ -0,0 +1,28 @@
+(load "tree.scm")
+
+;;; exercise 2.63
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list (right-branch tree)
+                                          result-list)))))
+  (copy-to-list tree '()))
+
+(define tree1 '(7 (3 (1 () ()) (5 () ())) (9 () (11 () ()))))
+(define tree2 '(3 (1 () ()) (7 (5 () ()) (9 () (11 () ())))))
+(define tree3 '(5 (3 (1 () ()) ()) (9 (7 () ()) (11 () ()))))
+
+;; Both procedures produce the same lists for all these trees.
+;; The time complexity for proc1 is O(n * log n), since append is O(n).
+;; The complexity of proc2 is O(n).
